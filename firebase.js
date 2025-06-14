f1f1d1d47dcffdb03bc295fc9c0d81876fd77aab
Matias Aguirre <matias.aguirre269@gmail.com>
2025-06-14 20:27:44 -0300

    actuializacion de seguridad

--- firebase.js
+++ firebase.js
@@ -1,15 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: import.meta.env.API_KEY,
-  authDomain: import.meta.env.AUTH_DOMAIN,
-  projectId: import.meta.env.PROJECT_ID,
-  storageBucket: import.meta.env.STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
-};
+import { firebaseConfig } from './firebase-config.js';
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAaZ57aRF0dlBv2MkLp8rZb2skpJvbEFl8",
+  authDomain: "gastocompartido.firebaseapp.com",
+  projectId: "gastocompartido",
+  storageBucket: "gastocompartido.firebasestorage.app",
+  messagingSenderId: "662478843777",
+  appId: "1:662478843777:web:6d659ddb7a4be42cc5c280"
+};
